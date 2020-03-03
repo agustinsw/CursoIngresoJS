2d1d029043c2098d8e6edc177232a01939442813
alumno <alumno@utnfra.com.ar>
2020-03-03 20:08:58 -0300

    blablabla

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
+    var numero = prompt("Ingrese un numero");
+    var numero = parseInt(numero);
 
+    while (isNaN(numero)){
+        numero = prompt("Ingrese un numero valido");
+        
+            numero = parseInt(numero);
+    }
+
+    for (var contador = 1; contador < numero; contador += 2) {
+        alert(contador + 1);
+
+    }
+//TERMINAR!
 
 
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var alumnoUnoNotas = prompt("Ingrese la nota del primer alumno");
+    var alumnoUnoNotas = parseInt(alumnoUnoNotas);
+    var alumnoUnoSexo = prompt ("Ingrese 'f' si es femenino y 'm' si es mascuino");
+
+    var alumnoDosNotas = prompt("Ingrese la nota del segundo alumno");
+    var alumnoDosNotas = parseInt(alumnoDosNotas);
+    var alumnoDosSexo = prompt ("Ingrese 'f' si es femenino y 'm' si es mascuino");
+
+    var alumnoTresNotas = prompt("Ingrese la nota del tercer alumno");
+    var alumnoTresNotas = parseInt(alumnoTresNotas);
+    var alumnoTresSexo = prompt ("Ingrese 'f' si es femenino y 'm' si es mascuino");
+
+    var alumnoCuatroNotas = prompt("Ingrese la nota del cuarto alumno");
+    var alumnoCuatroNotas = parseInt(alumnoCuatroNotas);
+    var alumnoCuatroSexo = prompt ("Ingrese 'f' si es femenino y 'm' si es mascuino");
+
+    var alumnoCincoNotas = prompt("Ingrese la nota del quinto alumno");
+    var alumnoCincoNotas = parseInt(alumnoCincoNotas);
+    var alumnoCincoSexo = prompt ("Ingrese 'f' si es femenino y 'm' si es mascuino");
 
 }
